dc41615749c5ef0a0fa082d5ba5a0019d8dc8518
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-11 21:28:34 -0600

    registerd clothing_items in admin

--- wardrobe_project/wardrobe/admin.py
+++ wardrobe_project/wardrobe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ClothingItem
+
+
+@admin.register(ClothingItem)
+class ClothingItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "description", "image_url", "user")
+    search_fields = ("id", "name", "category", "description", "image_url", "user")
+    list_filter = ("id", "user", "name", "category")
