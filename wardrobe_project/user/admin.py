6b406de8a9d4889b78ce18f0f562771aa2ed53c5
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-11 18:04:35 -0600

    created user modal and added to admin console

--- wardrobe_project/user/admin.py
+++ wardrobe_project/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "status")
+    search_fields = ("username", "first_name", "last_name", "email", "status")
+    list_filter = ("username", "first_name", "last_name")
--- wardrobe_project/user/models.py
+++ wardrobe_project/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("inactive", "Inactive"),
+    ]
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="active")
+    avatar_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email} {self.status}"
