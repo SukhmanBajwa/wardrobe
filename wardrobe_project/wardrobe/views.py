64762ec02ba6c6b7110ee5b14d347f67e2afca86
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-11 20:46:33 -0600

    ClothingItemViewSet created

--- wardrobe_project/wardrobe/views.py
+++ wardrobe_project/wardrobe/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ClothingItemSearializer
+from .models import ClothingItem
 
 # Create your views here.
+
+
+class ClothingItemViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ClothingItemSearializer
+
+    def get_queryset(self):
+        user = self.request.user
+        # pylint: disable=no-member
+        return ClothingItem.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
