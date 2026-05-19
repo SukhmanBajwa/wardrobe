7173cf0ee2fde2b71d340e1fe6e1c69dfde9cb2f
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-19 14:14:55 -0600

    simple two buttons for login and register

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,26 @@
-import Image from "next/image";
-import Register from "./register/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <main>
         <>
-          <Register></Register>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Link
+              href="/register"
+              className="text-sm text-gray-400 hover:text-gray-200 transition"
+            >
+              Register
+            </Link>
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Link
+              href="/login"
+              className="text-sm text-gray-400 hover:text-gray-200 transition"
+            >
+              Login
+            </Link>
+          </button>
         </>
       </main>
     </div>
