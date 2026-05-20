7d0abf2f334d323d9bf67044a2544039c4fd5dba
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-20 12:54:14 -0600

    category = serializers.StringRelatedField(

--- wardrobe_project/wardrobe/serializers.py
+++ wardrobe_project/wardrobe/serializers.py
@@ -4,6 +4,10 @@ from .models import ClothingItem
 
 
 class ClothingItemSearializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField(
+        many=False
+    )  # This will display the category name instead of the ID
+
     class Meta:
         model = ClothingItem
         fields = ["id", "name", "category", "description", "image_url", "user"]
