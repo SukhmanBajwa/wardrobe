b027a90cfe5d0030a25dd46f644b5cf7cdba2828
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-20 21:44:25 -0600

    added TagsAdmin and added inline tags editing capability

--- wardrobe_project/wardrobe/admin.py
+++ wardrobe_project/wardrobe/admin.py
@@ -2,11 +2,45 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ClothingItem
+from .models import ClothingItem, ClothingItemTag, Tag
+
+
+class ClothingItemTagInline(admin.TabularInline):
+    model = ClothingItemTag
+    extra = 1
 
 
 @admin.register(ClothingItem)
 class ClothingItemAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "category", "description", "image_url", "user")
-    search_fields = ("id", "name", "category", "description", "image_url", "user")
+
+    def tags(self, obj):
+        print("working")
+        return [row.tag.name for row in obj.item.all()]
+
+    inlines = [ClothingItemTagInline]
+
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "tags",
+        "description",
+        "image_url",
+        "user",
+    )
+    search_fields = (
+        "id",
+        "name",
+        "category",
+        "description",
+        "image_url",
+        "user",
+    )
     list_filter = ("id", "user", "name", "category")
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "source", "colour_hex")
+    search_fields = ("id", "name", "source", "colour_hex")
+    list_filter = ("id", "name", "source", "colour_hex")
