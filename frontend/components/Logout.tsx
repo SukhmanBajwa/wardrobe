109d7657865a9513af150f8d62a5f622fdaa9fd7
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-23 23:56:17 -0600

    logout comp

---
+++ frontend/components/Logout.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Logout() {
+  const router = useRouter();
+  useEffect(() => {
+    async function sendLogout() {
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + "/api/auth/logout/",
+        {
+          method: "POST",
+          credentials: "include",
+        },
+      );
+      if (res.ok) {
+        router.push("/login");
+      }
+    }
+    sendLogout();
+  });
+  return (
+    <>
+      <h1> Thank you for visiting. You have been logged out </h1>
+    </>
+  );
+}
