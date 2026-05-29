16c104e7ade8a4163d8466f2455ef9f0a7a75814
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-05-28 18:51:04 -0600

    category handling when making clothing item and category serializer

--- wardrobe_project/wardrobe/serializers.py
+++ wardrobe_project/wardrobe/serializers.py
@@ -7,28 +7,25 @@ class ClothingItemSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField(
         many=False
     )  # This will display the category name instead of the ID
-    set_category = serializers.CharField(
+    set_category = serializers.IntegerField(
         write_only=True, required=False
     )  # This will accept the name of category white POST
     tags = serializers.SerializerMethodField()
-
-    categories = Category.objects.all()
-    print(categories)
 
     def get_tags(self, obj):
         rows = obj.item.all()
         return [row.tag.name for row in rows]
 
-    def validate_category(self, value):
-        if value not in self.categories:
+    def validate_set_category(self, value):
+        if not Category.objects.filter(id=value).exists():
             raise serializers.ValidationError(
                 f"{value} is not found in list of available categories."
             )
         return value
 
     def create(self, validated_data):
-        category_name = validated_data.pop("set_category", None)
-        category = Category.objects.get(name=category_name) if category_name else None
+        category_id = validated_data.pop("set_category", None)
+        category = Category.objects.get(id=category_id) if category_id else None
         return ClothingItem.objects.create(category=category, **validated_data)
 
     class Meta:
@@ -44,3 +41,9 @@ class ClothingItemSerializer(serializers.ModelSerializer):
             "tags",
         ]
         read_only_fields = ["user"]
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
