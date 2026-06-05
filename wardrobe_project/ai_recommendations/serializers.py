d02fec72dee199ed71dc9d0ecc98c97e385a0a8d
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-06-04 23:32:05 -0600

    Logic to make sure recommendations are both ways

--- wardrobe_project/ai_recommendations/serializers.py
+++ wardrobe_project/ai_recommendations/serializers.py
@@ -5,6 +5,7 @@ from .models import AiRecommendation, AiDescription
 
 
 class AiRecommendationsSerilizer(serializers.ModelSerializer):
+    item = ClothingItemSerializer(read_only=True)
     recommended_item = ClothingItemSerializer(read_only=True)
 
     class Meta:
@@ -15,3 +16,17 @@ class AiRecommendationsSerilizer(serializers.ModelSerializer):
             "reason",
             "best_match",
         ]
+
+
+class AiRecommendationsBackwardSerilizer(serializers.ModelSerializer):
+    item = ClothingItemSerializer(read_only=True)
+    recommended_item = ClothingItemSerializer(read_only=True)
+
+    class Meta:
+        model = AiRecommendation
+        fields = ["item", "recommended_item", "reason", "best_match"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["item"], data["recommended_item"] = data["recommended_item"], data["item"]
+        return data
--- wardrobe_project/ai_recommendations/views.py
+++ wardrobe_project/ai_recommendations/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .serializers import AiRecommendationsSerilizer
+from django.db.models import Q
+
+from ai_recommendations.models import AiRecommendation
+from .serializers import AiRecommendationsSerilizer, AiRecommendationsBackwardSerilizer
 
 
 # Create your views here.
@@ -16,14 +19,34 @@ class RecommendationsAPIView(APIView):
         if item_id:
             item = get_object_or_404(ClothingItem, id=item_id)
             if item.recommendations.exists():
-                recommendations = item.recommendations.filter(
-                    recommended_item__is_deleted=False
+                recommendations = AiRecommendation.objects.filter(
+                    item=item,
+                    recommended_item__is_deleted=False,
                 )
-                if recommendations:
-                    return Response(
-                        AiRecommendationsSerilizer(recommendations, many=True).data,
-                        status=status.HTTP_200_OK,
-                    )
+                recommendations_backwards = AiRecommendation.objects.filter(
+                    recommended_item=item,
+                    recommended_item__is_deleted=False,
+                )
+                forward_data = AiRecommendationsSerilizer(
+                    recommendations, many=True
+                ).data
+                backwards_data = AiRecommendationsBackwardSerilizer(
+                    recommendations_backwards, many=True
+                ).data
+
+                seen = set()
+                unique = []
+
+                for r in list(forward_data) + list(backwards_data):
+                    rec_id = r["recommended_item"]["id"]
+                    if rec_id not in seen:
+                        seen.add(rec_id)
+                        unique.append(r)
+
+                return Response(
+                    unique,
+                    status=status.HTTP_200_OK,
+                )
         else:
             return Response(
                 {"Please provide item ID"}, status=status.HTTP_406_NOT_ACCEPTABLE
