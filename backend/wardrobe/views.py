9a1aa786de2f51016903fca8bed4c33b678485e4
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-06-12 12:10:58 -0600

    set ai recommendations task on different thread to avoid delay in viewing new item

--- backend/wardrobe/views.py
+++ backend/wardrobe/views.py
@@ -1,3 +1,5 @@
+import threading
+
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -14,7 +16,7 @@ class ClothingItemViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwner]
     serializer_class = ClothingItemSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ["name", "description", "item__tag__name"]
+    search_fields = ["name", "description", "tagged_item__tag__name"]
 
     def get_queryset(self):
         user = self.request.user
@@ -27,16 +29,22 @@ class ClothingItemViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(category__name=category)
         return queryset
 
-    # method override from CreateModelMixin to change the behaviour of save. Include user info
+    @staticmethod
+    def run_recommendations(new_item, inventory):
+        print("it ran")
+        ai_reply, _ = services.Ai_Recommendation(new_item, inventory)
+        services.Save_Ai_Recommendations(new_item.id, ai_reply)
 
+    # method override from CreateModelMixin to change the behaviour of save. Include user info
     def perform_create(self, serializer):
         new_item = serializer.save(user=self.request.user)
-        item_info = ClothingItemSerializer(new_item).data
-        inventory_data = ClothingItemSerializer(self.get_queryset(), many=True).data
-        ai_reply, response_status = services.Ai_Recommendation(
-            item_info, inventory_data
+
+        # making ai recommendation work behind the scene and let item appear in view instantly
+        thread = threading.Thread(
+            target=self.run_recommendations, args=(new_item, self.get_queryset())
         )
-        services.Save_Ai_Recommendations(new_item.id, ai_reply)
+        thread.daemon = True
+        thread.start()
 
     # method override from DestroyModelMixin to change behaviour to mark object "is_delete: True"
     def perform_destroy(self, instance):
