e0c7d0aa572173b212a8594b78339a92bf2e3333
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-07-02 12:52:05 -0600

    vercel url added to Allowed list

--- backend/wardrobe_project/settings.py
+++ backend/wardrobe_project/settings.py
@@ -198,7 +198,11 @@ STORAGES = {
 
 AUTH_USER_MODEL = "user.CustomUser"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://wardrobe-indol-theta.vercel.app",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
