efe68c662982935a657ac58811d812ede8dc60a1
Sukhmanpreet Singh <sukhman.bajwa@outlook.com>
2026-07-03 11:06:14 -0600

    logging

--- backend/wardrobe_project/settings.py
+++ backend/wardrobe_project/settings.py
@@ -235,16 +235,16 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "handlers": {
-#         "console": {
-#             "class": "logging.StreamHandler",
-#         },
-#     },
-#     "root": {
-#         "handlers": ["console"],
-#         "level": "DEBUG",
-#     },
-# }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
